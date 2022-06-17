ac1bd7a19bf6477cc9db50379083edbe90cbd04e
Dr Squanch <caadams@udel.edu>
2022-06-16 22:15:39 -0400

    test deployed

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-import * as firebase from './firebase';
+/*import * as firebase from '../firebase';
+import { create_user } from './helpers/register';
+import { sign_user_in } from './helpers/signin';
+*/
+$("#sign_in_user").submit(function () {
+    alert("Submitted");
+});
+
+$("#register_user").submit(function () {
+    alert("Submitted");
+});
