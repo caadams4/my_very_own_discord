ef046397bee2e9786bd64f42214f888eb4013659
dr_squanch <79220528+caadams4@users.noreply.github.com>
2022-09-23 11:09:10 -0400

    added code from glitch repo

--- helpers/render_app_data.js
+++ helpers/render_app_data.js
@@ -1,51 +1,57 @@
-import { demote_to_pleb, kick_from_server, promote_to_admin } from './user_handling.js';
+import {
+  demote_to_pleb,
+  kick_from_server,
+  promote_to_admin,
+} from "./user_handling.js";
 
 let firebase_object_here;
 let current_server;
 
 function render_servers(firebase_object, server_data_raw, user_info) {
-  let uid = user_info.uid;
-  let server_data = server_data_raw;
+  let server_data = server_data_raw.val();
+  console.log("SERVER LIST = ")
+  console.log(server_data)
   let serverIds = Object.keys(server_data);
   $(".servers").empty();
-  serverIds.map((name) => {
-    if (server_data[name].members[uid]) {
+  serverIds.map((id) => {
       if (user_info.role === "admin") {
         $(".servers").append(
           `<li  >
             <div class="server_list_item">
-              <a class="server_object"  data-id='${name}'>
-                ${name}
+              <a class="server_object"  data-id='${server_data[id]}' >
+                ${server_data[id]}
               </a>
          	    <div class="edit">
                 <a  href="#">
-                  <i id='${name}' title="Delete Server" class="fa fa-times fa-sm  text-danger" >
+                  <i id='${server_data[id]}_delete' title="Delete Server" class="fa fa-times fa-sm  text-danger" >
                   </i>
                 </a>
               </div>
             </div>
           </li>`
         );
-        $(`#${name}`).click(() => {
-          server_name_delete(name);
+        $(`#${server_data[id]}_delete`).click(() => {
+          server_name_delete(firebase_object,server_data[id]);
         });
       } else {
         $(".servers").append(
           `<li  >
             <div class="server_list_item">
-              <a class="server_object" data-id=${name}>
-                ${name}
+              <a class="server_object" data-id=${server_data[id]}>
+                ${server_data[id]}
               </a>
             </div>
           </li>`
         );
       }
-      $(".server_object").click(click_handler_server);
-    }
-  });
+      $(".server_object").click((evt)=>{
+        click_handler_server(firebase_object,evt);
+      });
+    });
+  
 }
 
-function server_name_delete(name) {
+function server_name_delete(firebase_object,name) {
   let are_you_sure = confirm(`Are you sure you want\nto remove ${name}?`);
   if (are_you_sure === true) {
     let titleRef = firebase_object_here.rtdb.ref(firebase_object_here.db, "/");
@@ -53,81 +59,103 @@ function server_name_delete(name) {
       titleRef,
       `chatServers/${name}/`
     );
+    
     firebase_object_here.rtdb.set(chatRefRemove, null);
   }
-  console.log(name);
 }
 
-let click_handler_server = function (evt) {
+let click_handler_server = function (firebase_object_here,evt) {
   let clickedElement = evt.currentTarget;
   let idFromDOM = $(clickedElement).attr("data-id");
+
   let userServerRef = firebase_object_here.rtdb.child(
     firebase_object_here.titleRef,
     `users/${firebase_object_here.auth.currentUser.uid}`
   );
-  firebase_object_here.rtdb.update(userServerRef, { server: idFromDOM });
-};
+  
+  let currentServerRef = firebase_object_here.rtdb.child(
+    firebase_object_here.titleRef,
+    `chatServers/${idFromDOM}/members/`);
+  
+  
+  firebase_object_here.rtdb.get(currentServerRef).then((data)=>{
+      let server_data = data.val();
+      let uids = Object.keys(server_data);
+      let has_access = false;
+      uids.forEach((uid) => {
+        if (uid === firebase_object_here.auth.currentUser.uid) { 
+          firebase_object_here.rtdb.update(userServerRef, { server: idFromDOM });
+          has_access = true;
+        }
+      
+    
+      });
+      if (!has_access) alert("You don't have access to this server");
+  })
+  }
 
-function render_messages(firebase_object, server_data_raw, current_server) {
-  let server_data = server_data_raw;
-  let messages = Object.keys(server_data[current_server].messages);
-  $(".messages").empty();
-  messages.map((each_message) => {
-    console.log(server_data[current_server].messages[each_message])
-    if (
-      server_data[current_server].messages[each_message].senderID ===
-      firebase_object.auth.currentUser.uid
-    ) {
-      $(".messages").append(
-        `<div class="message_from_me_parent">
+function render_messages(firebase_object, current_server) {
+  let serverMessagesRef = firebase_object.rtdb.child(
+    firebase_object.titleRef,
+    `chatServers/${current_server}/messages`
+  );
+  firebase_object.rtdb.onValue(serverMessagesRef, (message_data) => {
+    let message_object = message_data.val();
+    let messages = Object.keys(message_object);
+    $(".messages").empty();
+    messages.map((each_message) => {
+      if (
+        message_object[each_message].senderID ===
+        firebase_object.auth.currentUser.uid
+      ) {
+        $(".messages").append(
+          `<div class="message_from_me_parent">
           <div data-id='${each_message}' id='${each_message}' class="message_from_me">
             <div class="message_details">
-              ${server_data[current_server].messages[each_message].timeStamp}
+              ${message_object[each_message].timeStamp}
             </div>\
             <hr style="white" margin="6">
-            ${server_data[current_server].messages[each_message].message}
+            ${message_object[each_message].message}
             <div class="message_details">
-              @${server_data[current_server].messages[each_message].senderName}
+              @${message_object[each_message].senderName}
             </div>
         </div>`
-      );
-    } else {
-      $(".messages").append(
-        `<div data-id='${each_message}' class="message_not_from_me">
+        );
+      } else {
+        $(".messages").append(
+          `<div data-id='${each_message}' class="message_not_from_me">
             <div class="message_details">
-              ${server_data[current_server].messages[each_message].timeStamp}
+              ${message_object[each_message].timeStamp}
             </div>\
             <hr style="white" margin="6">
-            ${server_data[current_server].messages[each_message].message}
+            ${message_object[each_message].message}
             <div class="message_details">
-              @${server_data[current_server].messages[each_message].senderName}
+              @${message_object[each_message].senderName}
             </div>
         </div>`
-      );
-    }
+        );
+      }
+    });
   });
 }
 
 function render_users(
   firebase_object,
-  user_server_data,
-  server_data_raw,
+  admin_uids,
   current_server,
-   user_info
+  user_info
 ) {
-  let server_data = server_data_raw;
-  let serverIds = Object.keys(server_data);
-  let userIds = Object.keys(server_data[current_server].members);
-  let user_data = user_server_data;
+    console.log(current_server)
+  let userIds = Object.keys(current_server['members']);
   $(".users").empty();
-    if (user_info.role === "admin") {
-  userIds.map((userID) => {
-    let renderUser = server_data[current_server].members[userID].username;
-    if (user_data[userID].roles["admin"] === true) {
-      let demote = `${renderUser}` + "_demote";
-      $(".users").append(`
+  if (admin_uids.includes(user_info.uid)) {
+    userIds.map((userID) => {
+      let renderUser = current_server.members[userID].username;
+      if (admin_uids.includes(userID)) {
+        let demote = `${renderUser}` + "_demote";
+        $(".users").append(`
           <li data-id=${renderUser}>
-            <div class="user_list_item">
+            <div class="user_list_item" title='admin'>
               
               <a href="#">
               <i class="fa fa-user-circle" aria-hidden="true"></i>
@@ -141,17 +169,17 @@ function render_users(
             </div>
           </li>
         `);
-      $(`#${demote}`).click(() => {
-        demote_to_pleb(name, userID, firebase_object_here);
-      });
-    }
-  });
-  userIds.map((userID) => {
-    let renderUser = server_data[current_server].members[userID].username;
-    if (user_data[userID].roles["admin"] === false) {
-      let kick = `${renderUser}` + "_kick";
-      let promote = `${renderUser}` + "_promote";
-      $(".users").append(`
+        $(`#${demote}`).click(() => {
+          demote_to_pleb(name, userID, firebase_object_here);
+        });
+      }
+    });
+    userIds.map((userID) => {
+      let renderUser = current_server.members[userID].username;
+      if (!admin_uids.includes(userID)) {
+        let kick = `${renderUser}` + "_kick";
+        let promote = `${renderUser}` + "_promote";
+        $(".users").append(`
           <li data-id=${renderUser}>
             <div class="user_list_item">
               <a href="#">
@@ -168,20 +196,20 @@ function render_users(
             </div>
           </li>
         `);
-      $(`#${kick}`).click(() => {
-        kick_from_server(name, userID, current_server, firebase_object_here);
-      });
-      $(`#${promote}`).click(() => {
-        promote_to_admin(name, userID, firebase_object_here);
-      });
-    }
-  });
+        $(`#${kick}`).click(() => {
+          kick_from_server(name, userID, current_server, firebase_object_here);
+        });
+        $(`#${promote}`).click(() => {
+          promote_to_admin(name, userID, firebase_object_here);
+        });
+      }
+    });
   } else {
-  userIds.map((userID) => {
-    let renderUser = server_data[current_server].members[userID].username;
-    if (user_data[userID].roles["admin"] === true) {
-      let demote = `${renderUser}` + "_demote";
-      $(".users").append(`
+    userIds.map((userID) => {
+      let renderUser = current_server.members[userID].username;
+      if (admin_uids.includes(userID)) {
+        let demote = `${renderUser}` + "_demote";
+        $(".users").append(`
           <li data-id=${renderUser}>
             <div class="user_list_item">
             
@@ -192,17 +220,17 @@ function render_users(
             </div>
           </li>
         `);
-      $(`#${demote}`).click(() => {
-        demote_to_pleb(name, userID, firebase_object_here);
-      });
-    }
-  });
-  userIds.map((userID) => {
-    let renderUser = server_data[current_server].members[userID].username;
-    if (user_data[userID].roles["admin"] === false) {
-      let kick = `${renderUser}` + "_kick";
-      let promote = `${renderUser}` + "_promote";
-      $(".users").append(`
+        $(`#${demote}`).click(() => {
+          demote_to_pleb(name, userID, firebase_object_here);
+        });
+      }
+    });
+    userIds.map((userID) => {
+      let renderUser = current_server.members[userID].username;
+      if (!admin_uids.includes(userID)) {
+        let kick = `${renderUser}` + "_kick";
+        let promote = `${renderUser}` + "_promote";
+        $(".users").append(`
           <li data-id=${renderUser}>
             <div class="user_list_item">
               <a href="#">
@@ -211,15 +239,14 @@ function render_users(
             </div>
           </li>
         `);
-      $(`#${kick}`).click(() => {
-        kick_from_server(name, userID, current_server, firebase_object_here);
-      });
-      $(`#${promote}`).click(() => {
-        promote_to_admin(name, userID, firebase_object_here);
-      });
-    }
-  });
-    
+        $(`#${kick}`).click(() => {
+          kick_from_server(name, userID, current_server, firebase_object_here);
+        });
+        $(`#${promote}`).click(() => {
+          promote_to_admin(name, userID, firebase_object_here);
+        });
+      }
+    });
   }
 }
 
@@ -231,25 +258,55 @@ function get_user_server(firebase_object, user_data_raw, current_server) {
 }
 
 function database_listen_for_change(firebase_object, user_info) {
-  firebase_object_here = firebase_object;
-  firebase_object.rtdb.onValue(firebase_object.titleRef, (server_data) => {
-    let all_server_data = server_data.val();
-    let chat_server_data = all_server_data["chatServers"];
-    let user_server_data = all_server_data["users"];
+  let currentServerRef = firebase_object.rtdb.child(
+    firebase_object.titleRef,
+    `users/${firebase_object.auth.currentUser.uid}/server/`
+  );
+
+  firebase_object.rtdb.onValue(currentServerRef, (user_data) => {
+    let current_server = user_data.val();
+    render_messages(firebase_object, current_server);
+  });
+
+  currentServerRef = firebase_object.rtdb.child(
+    firebase_object.titleRef,
+    `chatServers/${user_info.server}/`
+  );
+  let roleRef = firebase_object.rtdb.child(
+    firebase_object.titleRef,
+    `groups/admins/`
+  );
+
+  firebase_object.rtdb.onValue(currentServerRef, (server_data) => {
+    let current_server = server_data.val()[user_info.server];
+    console.log(current_server)
+    firebase_object.rtdb.onValue(roleRef, (role_data) => {
+      let admins_data = role_data.val();
+      let admin_uids = Object.keys(admins_data)
+      render_users(
+        firebase_object,
+        admin_uids,
+        current_server,
+        user_info
+      );
+    });
+
+    /*
     current_server = get_user_server(
       firebase_object,
       user_server_data,
       current_server
     );
-    render_servers(firebase_object, chat_server_data, user_info);
-    render_users(
-      firebase_object,
-      user_server_data,
-      chat_server_data,
-      current_server,
-      user_info
-    );
-    render_messages(firebase_object, chat_server_data, current_server);
+    */
+  let serverListRef = firebase_object.rtdb.child(
+    firebase_object.titleRef,
+    `servers/`
+  );
+    firebase_object.rtdb.onValue(serverListRef, (server_list) => {
+      
+    render_servers(firebase_object, server_list, user_info);
+    });
+
     return current_server;
   });
   return current_server;
@@ -264,15 +321,16 @@ function build_user_info(firebase_object, user_info) {
   };
   firebase_object.rtdb.onValue(firebase_object.userRef, (user_data) => {
     let uid = firebase_object.auth.currentUser.uid;
+    console.log(firebase_object.auth.currentUser.uid);
     user_info.uid = uid;
-    console.log(user_data.val()[uid]);
+    // console.log(user_data.val()[uid]);
+    console.log(user_data.val());
     user_info.username = current_server = user_data.val()[uid].name;
     user_info.server = current_server = user_data.val()[uid].server;
     if (user_data.val()[uid].roles["admin"] === true) {
       user_info.role = "admin";
       $(".admin_tools").css({ display: "contents" });
     } else {
-      console.log(user_data.val()[uid].roles["admin"])
       user_info.role = "user";
       $(".admin_tools").css({ display: "none" });
     }
--- helpers/user_handling.js
+++ helpers/user_handling.js
@@ -1,17 +1,36 @@
-function demote_to_pleb(username, userID, firebase_object_here) {
+function demote_to_pleb(username, userID, firebase_object_here) { 
+  // for admins, demote other admin to user
+  
   let roleRef = firebase_object_here.rtdb.child(firebase_object_here.titleRef,`users/${userID}/roles/admin/`);
-  firebase_object_here.rtdb.set(roleRef,false);
+  firebase_object_here.rtdb.set(roleRef,false); // set target user admin role to false
+    let admin_group_ref = firebase_object_here.rtdb.ref(
+        firebase_object_here.db,
+        `/groups/admin/${firebase_object_here.auth.currentUser.uid}`
+      );
+      firebase_object_here.rtdb.set(admin_group_ref,null);
 }
 
-function kick_from_server(username, userID, current_server, firebase_object_here) {
+function kick_from_server(username, userID, current_server, firebase_object_here) { 
+  // for admins, kick user out of server
+  
   let memberRef = firebase_object_here.rtdb.child(firebase_object_here.titleRef,`chatServers/${current_server}/members/${userID}/`);
-  firebase_object_here.rtdb.set(memberRef,null);
+  firebase_object_here.rtdb.set(memberRef,null); // set target user membership to null
 }
 
 
-function promote_to_admin(username, userID, firebase_object_here) {
+function promote_to_admin(username, userID, firebase_object_here) { 
+  // for admins, promote user to server
+  
   let roleRef = firebase_object_here.rtdb.child(firebase_object_here.titleRef,`users/${userID}/roles/admin/`);
-  firebase_object_here.rtdb.set(roleRef,true);
+  firebase_object_here.rtdb.set(roleRef,true); // set target user admin role to true
+  let admin_group_ref = firebase_object_here.rtdb.ref(
+        firebase_object_here.db,
+        `/groups/admin/${firebase_object_here.auth.currentUser.uid}`
+      );
+      let admin_group = {
+        username,
+      };
+      firebase_object_here.rtdb.set(admin_group_ref, admin_group); // add user info to admin group
 }
 
 export { demote_to_pleb, kick_from_server, promote_to_admin };
